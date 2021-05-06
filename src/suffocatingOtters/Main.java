00a61a25234cc47138b247a8856bbc38741f7980
Andria Xu <axu436@student.fuhsd.org>
2021-05-05 21:03:18 -0700

    Update Main.java
    made it create a blank window

--- src/suffocatingOtters/Main.java
+++ src/suffocatingOtters/Main.java
@@ -1,7 +1,24 @@
 package suffocatingOtters;
 
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+
+import suffocatingOtters.graphics.AddPlayers;
+import suffocatingOtters.graphics.DrawingSurface;
+import suffocatingOtters.graphics.StartPage;
+
 public class Main {
 	public static void main(String args[]) {
+		DrawingSurface drawing = new DrawingSurface();
+		JFrame window = new StartPage();
 
+		window.setSize(400, 300);
+		window.setMinimumSize(new Dimension(100,100));
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setResizable(true);
+
+		window.setVisible(true);
 	}
 }
