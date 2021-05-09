c9f911f881d57c7c21de270da341fd729c0d7f16
Skyla Ma <sma703@student.fuhsd.org>
2021-05-08 17:21:20 -0700

    updating

--- src/theDeathNotice/Main.java
+++ src/theDeathNotice/Main.java
@@ -5,6 +5,7 @@ import java.awt.Dimension;
 import javax.swing.JFrame;
 
 import theDeathNotice.graphics.AddPlayers;
+import theDeathNotice.graphics.MainGame;
 import theDeathNotice.graphics.StartPage;
 
 /**
@@ -19,5 +20,30 @@ public class Main{
 		JFrame window = new StartPage();
 
 		window.setVisible(true);
+		try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+		} catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(MainGame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(MainGame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(MainGame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(MainGame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        //</editor-fold>
+
+        /* Create and display the form 
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new MainGame(playerNames).setVisible(true);
+            }
+        });*/
 	}
 }
