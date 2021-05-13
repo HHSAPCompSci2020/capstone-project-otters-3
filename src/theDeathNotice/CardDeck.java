98e446c047f58a1698e1cc3aab02fabb0b42be7f
Skyla Ma <sma703@student.fuhsd.org>
2021-05-13 13:01:49 -0700

    updating

--- src/theDeathNotice/CardDeck.java
+++ src/theDeathNotice/CardDeck.java
@@ -134,7 +134,7 @@ public class CardDeck {
 			return "dead";
 			
 	}
-	public static void main(String args[]) {
+//	public static void main(String args[]) {
 //		CardDeck c = new CardDeck();
 //		c.shuffleDeck();
 //		c.shuffleDeck();
@@ -152,5 +152,5 @@ public class CardDeck {
 //			}
 //		}
 //		// Game over, Declare winner
-	}
+//	}
 }
