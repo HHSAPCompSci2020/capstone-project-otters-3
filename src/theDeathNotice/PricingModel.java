7321d2a66021069beecbf551ac3aeffc4b28c04b
Skyla Ma <sma703@student.fuhsd.org>
2021-05-13 13:27:10 -0700

    javadocs for pricingModel

--- src/theDeathNotice/PricingModel.java
+++ src/theDeathNotice/PricingModel.java
@@ -19,12 +19,16 @@ public class PricingModel {
          
 	}
 	
-	// We borrowed the idea of Simulated annealing to dynamically adjust the price 
-	// of a saving card from time to time to keep the game going and more unpredictable and entertaining:
-	// 1. encourage everyone to buy saving card so the price will be less than the lowest point of 
-	//    all alive players who has points above 10m or above;
-	// 2. the shorter the distance from a dead card, the higher price of the saving cards;
-	// 3. price will be rounded to multiple of 5.
+	/** We borrowed the idea of Simulated annealing to dynamically adjust the price
+	 *  of a saving card from time to time to keep the game going and more unpredictable and entertaining:
+	 * 1. encourage everyone to buy saving card so the price will be less than the lowest point of 
+	 *    all alive players who has points above 10m or above;
+	 * 2. the shorter the distance from a dead card, the higher price of the saving cards;
+	 * 3. price will be rounded to multiple of 5.
+	 * 
+	 * This class calculates the new price of the SavingCard.
+	 * @return the new price of the SavingCard
+	 */
 	public int getPrice() {
 		
 		// Get distance from next dead card.
